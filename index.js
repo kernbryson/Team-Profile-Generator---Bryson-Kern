b5efc238ba39cef51c3cadbe03ea929a07f7b5ae
Bryson Kern <kernbryson@yahoo.com>
2022-05-06 13:57:30 -0400

    updated prompts for user input

--- index.js
+++ index.js
@@ -6,46 +6,58 @@ const Intern = require("./lib/intern");
 
 const inquirer = require("inquirer");
 const fs = require("fs");
+const { workerData } = require("worker_threads");
 // const generateMarkdown = require("./utils/generateMarkdown");
 
-const questions = [
-  {
-    type: "input",
+const teamSetup = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
 
-    name: "managerName",
+      name: "managerName",
 
-    message: "What is the team managers name?",
-  },
-  {
-    type: "input",
+      message: "What is the team managers name?",
+    },
+    {
+      type: "input",
 
-    name: "managerID",
+      name: "managerID",
 
-    message: "What is the team managers ID?",
-  },
-  {
-    type: "input",
+      message: "What is the team managers ID?",
+    },
+    {
+      type: "input",
 
-    name: "managerEmail",
+      name: "managerEmail",
 
-    message: "What is the team managers email?",
-  },
-  {
-    type: "input",
+      message: "What is the team managers email?",
+    },
+    {
+      type: "input",
 
-    name: "managerOfficeNumber",
+      name: "managerOfficeNumber",
 
-    message: "What is your team managers office number?",
-  },
-  {
-    type: "list",
+      message: "What is your team managers office number?",
+    },
+    {
+      type: "list",
 
-    name: "teamMembers",
+      name: "teamMembers",
 
-    message: "Would you like to add a team member?",
-    choices: ["yes", "No"],
-  },
-];
+      message: "Would you like to add a team member?",
+      choices: ["yes", "No"],
+    },
+  ])
+  .then(function(data){
+    console.log(data)
+    addMember(data.teamMembers);
+  });
+  
+};
+teamSetup();
+
+
+
 function addMember(wantedRole) {
   if (wantedRole === "yes") {
     inquirer.prompt([
@@ -55,12 +67,60 @@ function addMember(wantedRole) {
         message: "Please choose your employee's role",
         choices: ["Engineer", "Intern"],
       },
-    ]);
+    ])
+    .then(function(users){
+      console.log(users.role)
+      var selectedMember = users.role
+      if(selectedMember === 'Engineer'){
+       const engineer = new WorkerType ("Engineer");
+      }else{
+        const intern = new WorkerType ("Intern");
+      }
+    });
   } else {
     console.log("finished!");
   }
   
 }
+class workerType {
+  constructor(position) {
+    this.position = position;
+    this.id = id;
+    this.email = email;
+  }
+  workerData(){
+    const members = () => {
+      return inquirer.prompt([
+        {
+          type: "input",
+    
+          name: "memberID",
+    
+          message: `what is the ${this.position} name?`
+        }])
+        .then(function(users){
+          console.log(users)
+        });
+    }
+  }}
+  // let selectedMember = new WorkerType ('users.choices');
+//   }
+//   if(data.choices === "Engineer"){
+//       createEngineer()
+//   }else{
+//       createIntern()
+//   }
+// }
+// function createEngineer(){
+//     inquirer.prompt([
+//         {
+//             type: "input",
+//             name: "newEngineer",
+//             message: "what is your engineers name?",
+
+//           },
+//     ])
+// }
 
 // function writeToFile(data, fileName) {
 //   const createReadMe = generateMarkdown(data);
@@ -69,13 +129,11 @@ function addMember(wantedRole) {
 //   );
 // }
 
-function init() {
-  inquirer.prompt(questions).then((data) => {
-    console.log(data);
-    addMember(data.teamMembers);
+// function init() {
+//   inquirer.prompt(questions).then((data) => {
+//     console.log(data);
+//     addMember(data.teamMembers);
 
-    // writeToFile(data);
-  });
-}
-
-init();
+//     // writeToFile(data);
+//   });
+// }
